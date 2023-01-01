991f112aaddda6924b6b62a49d3003554bdc21e3
qaisma <iamqays@gmail.com>
2023-01-01 18:25:08 +0100

    rename project to sahenUI

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sahen-ui'),
+      dir: require('path').join(__dirname, './coverage/SahenUI'),
       subdir: '.',
       reporters: [
         { type: 'html' },
